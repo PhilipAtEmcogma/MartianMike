c710ec221fab5f5e98cdf8457939174a4f2cec60
PhilipAtEmcogma <emcogma@gmail.com>
2025-04-26 01:54:14 +1200

    update animation

--- martian-mike/scripts/player.gd
+++ martian-mike/scripts/player.gd
@@ -18,5 +18,22 @@ func _physics_process(delta: float) -> void:
 		
 	#returns -1 if first argument (move_left) is met, returns 1 if second argument (move_right) is met, returns 0 if neither or both is met
 	var direction = Input.get_axis("move_left", "move_right")
+	#use the direction to flip the sprite horizontally according to direction. so it face the direction it's going 
+	if direction !=0:
+		animated_sprite.flip_h = (direction == -1) #NOTE: flip_h is flip horizontal an actual function in the sprite object
 	velocity.x = direction * speed
 	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y <0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
