155b01f6a54ab166f0a78d289a5bf360e465c090
PhilipAtEmcogma <emcogma@gmail.com>
2025-04-26 02:07:13 +1200

    implement deathzone, and set camera limit to floor

--- martian-mike/scripts/level.gd
+++ martian-mike/scripts/level.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	body.velocity = Vector2.ZERO #reset player velocity
+	body.global_position = start_position.global_position #reset player global position to start position
