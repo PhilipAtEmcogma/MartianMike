ee499ea19529b955c3c3b04466006e8bc85f5c08
PhilipAtEmcogma <emcogma@gmail.com>
2025-04-28 02:36:47 +1200

    level timer and timer HUD implemented

--- martian-mike/scripts/level.gd
+++ martian-mike/scripts/level.gd
@@ -6,7 +6,16 @@ extends Node2D
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
 
+@onready var hud = $UILayer/HUD
+
 var player = null
+
+@export var level_time = 5
+
+var timer_node = null
+var time_left
+
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")#returns a single node
@@ -25,6 +34,26 @@ func _ready():
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
 	
+	time_left = level_time
+	hud.set_time_label(time_left)
+	
+	timer_node = Timer.new() #creating a new timer
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node) #add timer to the level scene
+	timer_node.start() #start the timer
+	
+func _on_level_timer_timeout():
+	#only continue to count down if plaher hasn't win yet.
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		#when ran out of time, reset the level and the time_left
+		if time_left <0:
+			reset_player()
+			time_left = level_time
+			hud.set_time_label(time_left)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -51,6 +80,7 @@ func _on_exit_body_entered(body):
 			#load next level, after playing animation
 			exit.animate()
 			player.active = false
+			win = true
 			await get_tree().create_timer(1.5).timeout #timeout for 1.5 sec before change scene, so animation can play
 			#packed is packed scene is Godot way to store scene files.
 			get_tree().change_scene_to_packed(next_level)
